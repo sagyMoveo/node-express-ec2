b972c8fc89bda3bf4878eb5d8d1f112bdf117e33
sagyMoveo <sagyb@moveo.co.il>
2023-09-19 16:51:27 +0300

    log added

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const app = express();
 
 app.listen(5001, () => console.log("Api running on port 5001..."));
 
-app.get("/", (req, res) => res.json("My API running! :)"));
+app.get("/", (req, res) => {
+  console.log("Server has been accessed");
+  res.json("My API running! :)");
+});
