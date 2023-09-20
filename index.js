86c1ecb6108b4fef270ee4593b1946c1a6fef979
sagyMoveo <sagyb@moveo.co.il>
2023-09-20 16:35:27 +0300

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-app.listen(5001, () => console.log("Api running on port 5001..."));
+app.listen(5002, () => console.log("Api running on port 5001..."));
 
 app.get("/hello", (req, res) => {
   const body = req.body;
